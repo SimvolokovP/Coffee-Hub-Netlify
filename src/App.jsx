f4e1b180f1e7c34863452bad12d46824791322d8
Alexey Svistunov <112976775+AlexSvistunov@users.noreply.github.com>
2024-10-30 11:14:00 +0300

    padding container

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,15 @@ import { useEffect } from 'react'
 import useTheme from './hooks/useTheme.js'
 import useTg from './hooks/useTg.js'
 
-
 const App = () => {
 	const { tg } = useTg()
 	const { checkThemeAndSetProperties } = useTheme()
 
-
 	const handleThemeChange = () => {
 		checkThemeAndSetProperties()
-    tg.MainButton.setParams({
-      color: document.documentElement.style.getPropertyValue('--accent-color'),
-    })
- 
+		tg.MainButton.setParams({
+			color: document.documentElement.style.getPropertyValue('--accent-color'),
+		})
 	}
 
 	const initialSetTheme = () => {
@@ -25,10 +22,8 @@ const App = () => {
 
 	useEffect(() => {
 		tg.onEvent('themeChanged', handleThemeChange)
-  
 
 		initialSetTheme()
-
 
 		return () => {
 			tg.offEvent('themeChanged', handleThemeChange)
