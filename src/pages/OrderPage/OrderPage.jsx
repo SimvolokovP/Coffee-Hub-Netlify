24e514fd57e8b07f980d82d26d9fab12b09ef7fa
Simvolokov Pavel <psimvolokov@gmail.com>
2024-11-02 19:58:19 +0300

    update ngrok

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -36,7 +36,7 @@ const OrderPage = () => {
       formData: formData,
     };
     console.log("Sending data:", data);
-    fetch(`${"https://5a65-95-32-137-53.ngrok-free.app"}/order`, {
+    fetch(`${"https://51cc-95-32-137-53.ngrok-free.app"}/order`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
